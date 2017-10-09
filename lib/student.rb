e747434cb6ee68eb374013d4bb82f87b64ad1d11
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 13:52:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       binding.pry
-      new_student = Student.new{student}
+      new_student = Student.new(student)
     end
   end
 
