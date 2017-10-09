1da372b1c3c0c833cb8f42f46edd33f63c0e9f5a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-09 13:20:36 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,28 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card")
+    student_info = students.collect do |student|
+      {:name => student.css(".student-name").text,
+      :location => student.css(".student-location").text,
+    :profile_url => student.css("a")[0]['href']}
+    end
+    #
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    scraped_profile = {:twitter => doc.css(".social-icon-container a")[0]['href'],
+    :linkedin => doc.css(".social-icon-container a")[1]['href'],
+    :github => doc.css(".social-icon-container a")[2]['href'],
+    :blog => doc.css(".social-icon-container a")[3]['href'],
+    :profile_quote => doc.css(".profile-quote").text,
+    :bio => doc.css(".bio-block p").text}
+    #binding.pry
+    scraped_profile
   end
 
 end
-
